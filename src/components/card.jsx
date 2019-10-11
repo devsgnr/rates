2f8ec6701ce2af3b8df13e7acb856950fb73e5de
Emmanuel Watila <itguru190@gmail.com>
2019-10-11 11:11:58 +0100

    Added currency formatting

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import accounting from "accounting-js";
 
 class Card extends Component {
   state = {};
@@ -7,9 +8,12 @@ class Card extends Component {
       <div className="col-sm-4">
         <div className="card mt-3 mb-2 p-5 border-0 shadow">
           <div className="d-flex flex-column justify-content-center align-items-center">
-            <h1 className="text-primary font-weight-bold">
-              {(this.props.rate * this.props.value).toFixed(2)}
-            </h1>
+            <div className="h2 text-primary font-weight-bold">
+              {accounting.formatMoney(
+                (this.props.rate * this.props.value).toFixed(2),
+                { symbol: this.props.currency.slice(3), format: "%v %s" }
+              )}
+            </div>
             <small className="font-weight-light text-muted pt-2 pb-2">
               {this.props.currency} - {this.props.rate}
             </small>
